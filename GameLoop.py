319b44c322188e46f57d4f32fd26191757690d2b
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-21 08:14:00 +0530

    Don

--- GameLoop.py
+++ GameLoop.py
@@ -5,4 +5,4 @@ pygame.init()
 screen = pygame.display.set_mode((300, 300)) #Height and the Width of the window
 screen.fill("white")
 
-pygame.display.set_caption("My First Python Game")
+pygame.display.set_caption("My First Python Game Program")
